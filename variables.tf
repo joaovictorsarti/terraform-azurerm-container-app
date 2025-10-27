f2094a8a9ef8ffb9f91904a3cfa5728527ea091a
João Silva <Joao-Silva@t-systems.com>
2025-10-27 13:48:42 -0300

    check module for container app

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ locals {
       id                                          = null
       dapr_application_insights_connection_string = null
       infrastructure_subnet_id                    = null
-      internal_load_balancer_enabled              = null
+      internal_load_balancer_enabled              = false
       zone_redundancy_enabled                     = null
       log_analytics_workspace_id                  = null
       tags                                        = {}
